ca37807193e4e8a168415bd9e475c15ce1cf59bf
barded1998 <barded1998@gmail.com>
2023-11-20 23:13:57 +0900

    refactor: user, short 리팩토링

--- src/main/java/com/ssafy/banjjak/shorts/controller/ShortController.java
+++ src/main/java/com/ssafy/banjjak/shorts/controller/ShortController.java
@@ -1,8 +1,10 @@
 package com.ssafy.banjjak.shorts.controller;
 
+import com.ssafy.banjjak.global.BaseResponse;
 import com.ssafy.banjjak.shorts.model.SearchCond;
 import com.ssafy.banjjak.shorts.model.ShortDto;
 import com.ssafy.banjjak.shorts.service.ShortService;
+import com.ssafy.banjjak.user.model.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,22 +20,22 @@ public class ShortController {
     private final ShortService shortService;
 
     @GetMapping
-    public ResponseEntity<List<ShortDto>> findBySearchCond(@RequestParam int pageNo,
-                                                           @RequestParam int amount,
-                                                           @RequestParam(required = false) Long userId,
-                                                           @RequestParam(required = false) String region) {
+    public ResponseEntity<BaseResponse<List<ShortDto>>> findBySearchCond(@RequestParam int pageNo,
+                                                                        @RequestParam int amount,
+                                                                        @RequestParam(required = false) Long userId,
+                                                                        @RequestParam(required = false) String region) {
         SearchCond searchCond = new SearchCond();
         searchCond.setPageNo(pageNo);
         searchCond.setAmount(amount);
         searchCond.setUserId(userId);
         searchCond.setRegion(region);
-        return ResponseEntity.status(HttpStatus.OK).body(shortService.findBySearchCond(searchCond));
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>("short를 가져왔습니다.", shortService.findBySearchCond(searchCond)));
     }
 
     @PostMapping
-    public ResponseEntity<?> createShort(@RequestBody ShortDto shortDto) {
+    public ResponseEntity<BaseResponse<?>> createShort(@RequestBody ShortDto shortDto) {
         shortService.createShort(shortDto);
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseResponse<>("short를 생성했습니다.", null));
     }
 
 }
